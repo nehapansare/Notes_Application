591f18258cded12d0efcbf180581da05a114651f
nehapansare <nehapansare007@gmail.com>
2024-09-26 15:50:37 +0530

    path updated

--- src/view/Show/Show.js
+++ src/view/Show/Show.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './show.css';
+import './Show.css';
 import { Link } from 'react-router-dom';
 import NoteCard from '../../compoent/NoteCard/NoteCard';
 
