d05a4354350232d490ba14889d4f0b64f0e379f3
nehapansare <nehapansare007@gmail.com>
2024-09-26 15:40:21 +0530

    card config added

---
+++ src/Config/card.js
@@ -0,0 +1,9 @@
+const cards = [
+    { imgUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwZIsTnr25tlJm_ksVEdHUGCSrMt13Ye7swQ&s', content: 'Search for specific content quickly!' },
+    { imgUrl:'https://cdn-icons-png.flaticon.com/512/4021/4021693.png', content: 'Enhance productivity with reminders and checklists!' },
+    {imgUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2vGv9eAAemT90lFszPHidcFhXiFmup6t2_A&s', content: 'Secure sensitive information!' },
+  ];
+
+  export{
+    cards
+  }
