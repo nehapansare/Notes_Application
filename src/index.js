fce9b7f94e8dbd84efdb603935d014629a57db9b
nehapansare <nehapansare007@gmail.com>
2024-09-26 15:52:26 +0530

    path updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './view/Home/Home'; 
+import Add from './view/Add/Add';
+import Show from './view/Show/Show';
+import { Toaster } from 'react-hot-toast';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/add",
+    element: <Add/>
+  },
+  {
+    path: "/show",
+    element: <Show/>
+  },
+  {
+    path:"*",
+    element:<h1>404 not found</h1>
+  }
+]);
+
+root.render(
+  <div>
+<RouterProvider router={router}/>
+<Toaster/>
+</div>
+);
