07a06f4f41274c57e8cdd6f1b7797f25af7ca49f
nehapansare <nehapansare007@gmail.com>
2024-09-27 15:41:48 +0530

    delte function added

--- src/compoent/NoteCard/NoteCard.js
+++ src/compoent/NoteCard/NoteCard.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import "./NoteCard.css";
 
-function NoteCard({ title, description, category }) {
+function NoteCard({ title, description, category,onDelete }) {
   return (
     <div className="note-card">
       <div className="note-card-content">
         <div className="note-card-header">
           <h1>{title}</h1>
+          <span className="category ">{category}</span>
         </div>
         <div className="note-card-body">
           <p>{description}</p>
-          <span className="category">{category}</span>
+        </div>
+        <div className='cat-container'>
+        <button onClick={onDelete} className="delete-button">Delete</button>
         </div>
       </div>
     </div>
